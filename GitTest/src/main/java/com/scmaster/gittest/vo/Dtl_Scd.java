1764b2f25522f6cd55d377fba2085c9d57c1f156
kita <kita@kita-PC>
2017-04-11 21:03:46 +0900

    SC13 도시검색 기능 완료. sql문 변경됨.

--- GitTest/src/main/java/com/scmaster/gittest/vo/Dtl_Scd.java
+++ GitTest/src/main/java/com/scmaster/gittest/vo/Dtl_Scd.java
@@ -9,11 +9,14 @@ public class Dtl_Scd {
 	private String memo;
 	private String place_nm;
 	private String dtl_image;
+	private String area_code;
+	private String sigungu_code;
+	private String city_nm;
 	
 	public Dtl_Scd(){}
 
 	public Dtl_Scd(int dtl_sq, int scd_sq, int daily_sq, int dtl_ord, String dtl_content_id, String memo,
-			String place_nm, String dtl_image) {
+			String place_nm, String dtl_image, String area_code, String sigungu_code, String city_nm) {
 		super();
 		this.dtl_sq = dtl_sq;
 		this.scd_sq = scd_sq;
@@ -23,6 +26,9 @@ public class Dtl_Scd {
 		this.memo = memo;
 		this.place_nm = place_nm;
 		this.dtl_image = dtl_image;
+		this.area_code = area_code;
+		this.sigungu_code = sigungu_code;
+		this.city_nm = city_nm;
 	}
 
 	public int getDtl_sq() {
@@ -89,10 +95,35 @@ public class Dtl_Scd {
 		this.dtl_image = dtl_image;
 	}
 
+	public String getArea_code() {
+		return area_code;
+	}
+
+	public void setArea_code(String area_code) {
+		this.area_code = area_code;
+	}
+
+	public String getSigungu_code() {
+		return sigungu_code;
+	}
+
+	public void setSigungu_code(String sigungu_code) {
+		this.sigungu_code = sigungu_code;
+	}
+
+	public String getCity_nm() {
+		return city_nm;
+	}
+
+	public void setCity_nm(String city_nm) {
+		this.city_nm = city_nm;
+	}
+
 	@Override
 	public String toString() {
 		return "Dtl_Scd [dtl_sq=" + dtl_sq + ", scd_sq=" + scd_sq + ", daily_sq=" + daily_sq + ", dtl_ord=" + dtl_ord
-				+ ", dtl_content_id=" + dtl_content_id + ", memo=" + memo + ", city_nm=" + place_nm + ", dtl_image=" + dtl_image
+				+ ", dtl_content_id=" + dtl_content_id + ", memo=" + memo + ", place_nm=" + place_nm + ", dtl_image="
+				+ dtl_image + ", area_code=" + area_code + ", sigungu_code=" + sigungu_code + ", city_nm=" + city_nm
 				+ "]";
 	}
 	
